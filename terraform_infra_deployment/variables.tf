fbc8c65e70e5b3c91201e02f9f9e8af2c0265db6
Parikshit Chavan <69300831+parikshit-2706@users.noreply.github.com>
2023-07-12 12:55:04 +0530

    Update variables.tf

--- terraform_infra_deployment/variables.tf
+++ terraform_infra_deployment/variables.tf
@@ -1,7 +1,7 @@
 
 variable "aws_region" {
   description = "AWS region to launch servers."
-  default     = "us-west-2"
+  default     = "us-east-1"
 }
 variable "env" {
   description = "Targeted Depolyment environment"
@@ -16,47 +16,19 @@ variable "nodejs_project_repository_branch" {
   default     = "master"
 }
 
-variable "python_project_repository_name" {
-  description = "Python Project Repository name to connect to"
-  default     = "pythonapp"
-}
-
-variable "python_project_repository_branch" {
-  description = "Python Project Repository branch to connect to"
-  default     = "master"
-}
-
-variable "golang_project_repository_name" {
-  description = "Go Lang Project Repository name to connect to"
-  default     = "goapp"
-}
-variable "golang_project_repository_branch" {
-  description = "Python Project Repository branch to connect to"
-  default     = "master"
-}
 
 variable "artifacts_bucket_name" {
   description = "S3 Bucket for storing artifacts"
-  default     = "your-project-name-cicd-artifacts-bucket"
+  default     = "ecs-cicd"
 }
 
 variable "aws_ecs_cluster_name" {
   description = "Target Amazon ECS Cluster Name"
-  default     = "MicroServicesCluster"
+  default     = "cluster2"
 }
 
 variable "aws_ecs_node_app_service_name" {
   description = "Target Amazon ECS Cluster NodeJs App Service name"
   default     = "nodeAppService"
-}
-
-variable "aws_ecs_python_app_service_name" {
-  description = "Target Amazon ECS Cluster Python App Service name"
-  default     = "pythonAppService"
-}
-
-variable "aws_ecs_go_app_service_name" {
-  description = "Target Amazon ECS Cluster Go App Service name"
-  default     = "goAppService"
 }
 
